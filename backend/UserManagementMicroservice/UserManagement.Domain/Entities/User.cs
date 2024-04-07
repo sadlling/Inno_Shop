9058af588fe2005600bff4388e5ceb294876f869
sadlling <iharzhilnski@gmail.com>
2024-04-07 14:34:03 +0300

    added user entity

---
+++ backend/UserManagementMicroservice/UserManagement.Domain/Entities/User.cs
@@ -0,0 +1,17 @@
+﻿using UserManagement.Domain.Common;
+
+namespace UserManagement.Domain.Entities
+{
+    public class User:BaseEntity
+    {
+        public string UserName { get; set; } = string.Empty!;
+        public string Email { get; set; } = string.Empty!;
+        public bool IsEmailConfirmed { get; set; }
+        public string PasswordHash { get; set; } = string.Empty!;
+        public string? PhoneNumber { get; set; }
+        public string? RefreshToken {  get; set; }
+        public DateTimeOffset? TokenCreated {  get; set; }
+        public DateTimeOffset? TokenExpires { get; set; }
+
+    }
+}
